3db8455db756082a627970693e602516b011b221
Tristan Garcia <you@example.com>
2022-12-07 13:59:40 +0100

    added some js testing code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const icon = document.querySelector(".fa");
 const list = document.querySelector(".navlistcontainer");
 const divBlur = document.querySelector(".navblur");
+//const pizza = document.querySelector(".pizza");
+
 
 icon.addEventListener("click", function () {
   list.classList.toggle("list-visible");
@@ -11,3 +13,6 @@ divBlur.addEventListener("click", function () {
   list.classList.toggle("list-visible");
   icon.classList.toggle("icon-visible");
 });
+/*pizza.addEventListener("click", function () {
+  pizza.classList.toggle("pizza-pop");
+})*/
