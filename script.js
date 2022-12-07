0a83a0d086e889528553ea1edbe282465e9e8554
Tristan Garcia <you@example.com>
2022-12-07 17:06:55 +0100

    tried something in js and css to make my pizza elements pop on click

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const icon = document.querySelector(".fa");
 const list = document.querySelector(".navlistcontainer");
 const divBlur = document.querySelector(".navblur");
-//const pizza = document.querySelector(".pizza");
-
+const pizza = document.querySelectorAll(".pizza");
+const zoomscreen = document.querySelector(".zoomscreen");
+ 
 
 icon.addEventListener("click", function () {
   list.classList.toggle("list-visible");
@@ -13,6 +14,14 @@ divBlur.addEventListener("click", function () {
   list.classList.toggle("list-visible");
   icon.classList.toggle("icon-visible");
 });
-/*pizza.addEventListener("click", function () {
-  pizza.classList.toggle("pizza-pop");
-})*/
+  for (let i = 0; i < pizza.length; i++) {
+    pizza[i].onclick = function () {
+      console.log(pizza[i].id);
+      pizza[i].classList.toggle("pizza-pop");
+      zoomscreen.classList.toggle("zoomscreen-visible");
+     let element = document.getElementById[i];
+      element.appendChild("zoomscreen");
+    }
+    
+  }
+  
